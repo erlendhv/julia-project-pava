b381d777d1268b79d1886efd34b544c6d0919695
Erlend Veire <erlend.veire@gmail.com>
2025-03-14 11:57:09 +0000

    added functions to file

--- Exceptional/src/Exceptional.jl
+++ Exceptional/src/Exceptional.jl
@@ -3,3 +3,28 @@ module Exceptional
 greet() = print("Hello World!")
 
 end # module Exceptional
+
+print("Hello World!")
+
+function to_escape(func)
+end
+
+function handling(func, handlers)
+end
+
+function with_restart(func, restarts)
+end
+
+function available_restart(name)
+end
+
+function invoke_restart(name, args)
+end
+
+function signal(exception)
+end
+
+function error(exception)
+end
+
+
